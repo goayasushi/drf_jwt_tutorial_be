24c7c6db47c4a02ee9fac382e8ac98ea098a4e88
goayasushi <goayasushi@gmail.com>
2025-01-23 08:51:56 +0900

    Snippet model

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)  # 作成日時（自動設定）
+    title = models.CharField(max_length=100, blank=True, default="")  # タイトル（任意）
+    code = models.TextField()  # コードスニペット本体
+    linenos = models.BooleanField(default=False)  # 行番号を表示するかどうか
+    language = models.CharField(default="python", max_length=100)  # プログラミング言語
+
+    class Meta:
+        ordering = ["created"]  # 作成日時で並び替え
