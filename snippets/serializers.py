657031805c772485e1f437b291b254ae7069339c
goayasushi <goayasushi@gmail.com>
2025-01-26 20:36:06 +0900

    snippetにuserを紐付け

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,12 +3,8 @@ from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = Snippet  # 使用するモデルを指定
-        fields = [
-            "id",
-            "title",
-            "code",
-            "linenos",
-            "language",
-        ]
+        fields = ["id", "title", "code", "linenos", "language", "owner"]
